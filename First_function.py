2f224836bdd951800cae24845df47e96cf7eb0f2
nomedousuario <cristiantcjti@gmail.com>
2021-02-04 11:33:48 -0300

    adding files

---
+++ First_function.py
@@ -0,0 +1,8 @@
+def Hi(name):
+    if name == "Cris":
+        show = "Hi "+name+". How's it going?"
+        print(show)
+    else:
+        print("Sorry, yor are not Cris...I do not talk to strangers")
+
+Hi('Cris')    
---
+++ For.py
@@ -0,0 +1,15 @@
+def Hi(name):
+    print("Hi "+name+". How's it going?")
+
+
+friends = ['Cristian', 'Marcos', 'Fernando', 'Dani', 'Cris']
+for name in friends: 
+    Hi(name)  
+    print("Our next friend is...")
+    print('')  
+
+print("Now let us print a range of numbers:")
+
+for num in range(1,10):
+    print(num)
+
