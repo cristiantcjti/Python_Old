c33be746b1d33809313f12b2a9104f08a5c7ecfe
cristian <cristiantcjti@gmail.com>
2021-03-11 11:11:31 -0300

    Classes

--- Classes/79char.py
+++
@@ -1,4 +0,0 @@
-story =  ("Once upon a time there was a very long string that was "
-          "over 100 characters long and could not all fit on the "
-          "screen at once.")
-print(story)
--- Classes/my_script.py
+++ Classes/my_script.py
@@ -1,7 +1,7 @@
 print("I run all the time!")
 
 if __name__ == '__main__':
-    print("I've been run directly!")
+    print("I've been running directly!")
 
 if __name__ == 'my_script':
     print("I've been imported!")
