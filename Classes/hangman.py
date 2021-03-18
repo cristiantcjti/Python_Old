d15dae15fd5d5187091729be53652af05ea7f613
cristian <cristiantcjti@gmail.com>
2021-03-18 19:42:00 -0300

    Hangman game

--- Classes/hangman.py
+++ Classes/hangman.py
@@ -4,6 +4,40 @@ def play_hangman():
     print("Welcome to the Hangman Game")
     print("****************************")
 
+    secret_word = "strawberry".upper()
+    hidden_letters = ["_ " for letter in secret_word]
+    guess_letter = ''
+    letter = ''
+    errors = 0
+    attempts = 5
+    loose = False
+    got_correct = False
+
+    while (not loose and not got_correct ):
+        index = 0
+        guess_letter = input("Guess a letter: ").strip().upper()
+        if (guess_letter in secret_word):
+            for letter in secret_word:
+                if guess_letter == letter:
+                    hidden_letters[index] = letter
+                index = index + 1
+        else:
+            errors += 1
+        
+        print(hidden_letters)
+        if (errors == attempts):
+            loose = True
+            break
+        if ("_ " not in hidden_letters):
+            got_correct = True
+            break
+        
+
+    if(loose): 
+        print(" You lost!") 
+    elif(got_correct):
+        print("You got it!")
+    
     print("End of game!")
 
 if(__name__ == "__main__"):
