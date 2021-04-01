5fb2581a906c13f8b38aa598b82c23dd81408199
cristian <cristiantcjti@gmail.com>
2021-04-01 16:25:54 -0300

    OOP_2

---
+++ Classes/OOP2/modelo.py
@@ -0,0 +1,58 @@
+class Program:
+    def __init__(self, name, year):
+        self._name = name.title()
+        self.year = year
+        self._likes = 0 
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name.title()
+    
+    @property
+    def likes(self):
+        return self._likes
+
+    def give_like(self):
+        self._likes += 1  
+
+    def __str__(self):
+        return f"Name: {self.name} - Year: {self.year} - Likes: {self.likes}."
+
+
+class Movie(Program):
+    def __init__(self, name, year, duration):
+        super().__init__("spiderman", 2013)
+        self.duration = duration
+    
+    def __str__(self):
+        return f"Name: {self.name} - Year: {self.year} -  Duration: {self.duration} - Likes: {self.likes}."
+
+
+class Series(Program):
+    def __init__(self, name, year, seasons):
+        super().__init__("atlanta", 2016)
+        self.seasons = seasons
+    
+    def __str__(self):
+        return f"Name: {self.name} - Year: {self.year} -  Season: {self.seasons} - Likes: {self.likes}."
+
+
+
+
+spiderman = Movie("spiderman", 2003, 120)
+spiderman.give_like()
+spiderman.give_like()
+spiderman.give_like()
+
+atlanta = Series("atlanta", 2017, 2)
+atlanta.give_like()
+
+
+movies_series = [spiderman, atlanta]
+
+for program in movies_series:
+    print(program)
