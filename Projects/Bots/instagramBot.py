77df8e8ac4c3924c8a1be1e82c6b111c96e5e394
cristian <cristiantcjti@gmail.com>
2021-04-13 20:29:09 -0300

    InstagramBot

--- Projects/Bots/instagramBot.py
+++ Projects/Bots/instagramBot.py
@@ -48,8 +48,5 @@ class InstagramBot:
                 time.sleep(5)
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-
-
-
 crisBot = InstagramBot('ti_tests','letscreateabot')
 crisBot.login()
