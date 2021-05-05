c28dc91f8c4f257633136ad79a6ded49a5511002
cristian <cristiantcjti@gmail.com>
2021-05-05 18:39:40 -0300

    Admin

--- Projects/cadastro_de_paciente/core/admin.py
+++ Projects/cadastro_de_paciente/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Patient
+from .models import Patient, Insurance
 
 admin.site.register(Patient)
+admin.site.register(Insurance)
