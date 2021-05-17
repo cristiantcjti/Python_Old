a17ef59cf63e3f114be1f98d9b0b2ab9fc89a0c0
cristian <cristiantcjti@gmail.com>
2021-05-17 15:37:55 -0300

    TDD

--- Classes/TDD/dominio.py
+++ Classes/TDD/dominio.py
@@ -22,22 +22,20 @@ class Leilao:
     def __init__(self, descricao):
         self.descricao = descricao
         self.__lances = []
-
-    @property
-    def lances(self):
-        return self.__lances
-
-class Avaliador:
-
-    def __init__(self):
         self.maior_lance = sys.float_info.min
         self.menor_lance = sys.float_info.max
 
-    def avalia(self, leilao: Leilao):
-        
-        for lance in leilao.lances:
+    def propoe(self, lance: Lance): # lance: Lance -> no python3 é possivel indicar o que esse method espera, dessa forma.
+        if not self.__lances or self.__lances[-1].usuario != lance.usuario and lance.valor > self.__lances[-1].valor:      
             if lance.valor > self.maior_lance:
                 self.maior_lance = lance.valor
-            elif lance.valor < self.menor_lance:
+            if lance.valor < self.menor_lance:
                 self.menor_lance = lance.valor
+            self.__lances.append(lance)
+        else:
+            raise ValueError("Erro ao propor o lance.")
+
+    @property
+    def lances(self):
+        return self.__lances[:] # [:] é uma forma de copiar uma lista.
 
---
+++ Classes/TDD/test_leilao.py
@@ -0,0 +1,89 @@
+from unittest import TestCase
+from dominio import Usuario, Lance, Leilao
+
+# method setUp // chamado antes de cada função
+# method tearDown // chamado logo após execução do teste
+# method setUpClass // chamado no momento da criação da classe
+# method tearDownClass // chamado após o último teste da classe ser executado
+
+#Classe de equivalencia, quando os testes são parecidos, apenas criamos situacoes diferentes
+class TestLeilao(TestCase):
+    # CRIAÇÃO DE CENÁRIO DE TESTES, QUANDO ALGO É REPETIDO EM TODOS OS PONTOS DE TESTE.  
+    def setUp(self): 
+        self.paulo = Usuario('paulo')  
+        self.lance_do_paulo = Lance(self.paulo, 150.0)    
+        self.leilao = Leilao('Celular')          
+
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
+        cris = Usuario('cris')
+        lance_do_cris = Lance(cris, 100.0)
+
+        self.leilao.propoe(lance_do_cris)
+        self.leilao.propoe(self.lance_do_paulo)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+    def test_nao_deve_permitir_propor_um_lance_em_ordem_decrescente(self):
+        
+        with self.assertRaises(ValueError):
+            cris = Usuario('cris')
+            lance_do_cris = Lance(cris, 100.0)
+            
+            self.leilao.propoe(self.lance_do_paulo)
+            self.leilao.propoe(lance_do_cris)
+
+    def test_deve_retornar_o_mesmo_valor_para_o_mairo_e_menor_lance_quando_leilao_tiver_um_lance(self):
+        self.leilao.propoe(self.lance_do_paulo)
+
+        self.assertEqual(150.0, self.leilao.menor_lance)
+        self.assertEqual(150.0, self.leilao.maior_lance)
+
+    def test_deve_retornar_o_mesmo_valor_para_o_mairo_e_menor_lance_quando_leilao_tiver_tres_lances(self):
+        cris = Usuario('cris')
+        marcos = Usuario('marcos')
+        
+        lance_do_cris = Lance(cris, 100.0)
+        lance_do_marcos = Lance(marcos, 200.0)
+
+        self.leilao.propoe(lance_do_cris)
+        self.leilao.propoe(self.lance_do_paulo)
+        self.leilao.propoe(lance_do_marcos)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 200.0
+
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance) 
+
+    def test_deve_permitir_propor_um_lance_caso_o_leilao_nao_tenha_lances(self):
+        self.leilao.propoe(self.lance_do_paulo)
+
+        quantidade_de_lances_recebido = len(self.leilao.lances)
+        self.assertEqual(1, quantidade_de_lances_recebido)
+
+    def test_deve_permitir_propor_um_lance_caso_o_ultimo_usuario_seja_diferente(self):
+        joao = Usuario('joan')
+
+        lance_do_joao = Lance(joao, 200.0)
+
+        self.leilao.propoe(self.lance_do_paulo)
+        self.leilao.propoe(lance_do_joao)
+
+        quantidade_de_lances_recebidos = len(self.leilao.lances)
+        self.assertEqual(2, quantidade_de_lances_recebidos)
+
+    def test_nao_deve_permitir_propor_lance_caso_o_usuario_seja_o_mesmo(self):
+        lance_do_paulo200 = Lance(self.paulo, 200.0)
+
+        with self.assertRaises(ValueError):
+            self.leilao.propoe(self.lance_do_paulo)
+            self.leilao.propoe(lance_do_paulo200)
+            quantidade_de_lances_recebidos = len(self.leilao.lances)
+            self.assertEqual(1, quantidade_de_lances_recebidos) 
+
+
